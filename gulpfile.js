d6b6ad76ec5a465896de26a6ad549ebc0e1d1e5b
Jamie Mansfield <dev@jamierocks.uk>
2016-07-30 14:04:40 +0100

    Use scss for stylesheets

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename')
+
+gulp.task('watch', ['build'], function () {
+    gulp.watch('./src/scss/**/*.scss', ['build']);
+});
+
+gulp.task('scss', function () {
+    gulp.src('./src/scss/homepage.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/assets/css'));
+
+    return gulp.src('./src/scss/base.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/assets/css'));
+});
+
+gulp.task('build', ['scss'], function () {
+    gulp.src('./public/assets/css/homepage.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./public/assets/css'));
+
+    return gulp.src('./public/assets/css/base.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./public/assets/css'));
+});
+
+gulp.task('default', ['build'], function () {
+
+});
